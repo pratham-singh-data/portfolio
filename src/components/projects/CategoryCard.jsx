bbebc993094f5dc0b85fea83c45215682ee96767
pratham-singh-data <PrathamSinghCE@gmail.com>
2022-08-06 02:09:17 +0530

    Filter funtionality added

--- src/components/projects/CategoryCard.jsx
+++ src/components/projects/CategoryCard.jsx
@@ -1,10 +1,19 @@
 import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material'
 import React from 'react'
 
-export const CategoryCard = ({data}) => {
+export const CategoryCard = ({data, setFilter}) => {
+  const cardClicked = () => {
+    if(data.id === 0){
+      setFilter(null);
+      return;
+    }
+
+    setFilter(data.id);
+  }
+
   return (
     <Box>
-        <Card sx={{width: "150px", height: "200px", padding: "10px", mr: "20px", cursor: "pointer", border: "3px solid #ffffff", "&:hover": {border: "3px solid #5e17eb"}, "&:active": {border: "3px solid #00ff00"}}}>
+        <Card sx={{width: "150px", height: "200px", padding: "10px", mr: "20px", cursor: "pointer", border: "3px solid #ffffff", "&:hover": {border: "3px solid #5e17eb"}, "&:active": {border: "3px solid #00ff00"}}} onClick={cardClicked}>
             <CardMedia component="img" image={data.logo} height="120px" alt={data.name} />
             <CardContent sx={{display: "flex", flexDirection: "row", justifyContent: "center", mt: "10px", color: "#272727"}}>
                 <Typography>{data.name}</Typography>
--- src/components/projects/ProjectsPage.jsx
+++ src/components/projects/ProjectsPage.jsx
@@ -1,15 +1,27 @@
 import { Box, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { CategoriesCarousel } from './CategoriesCarousel'
 import { WorkData } from '../../assets/data/work'
 import { ResultsShowcase } from './ResultsShowcase'
 
 export const ProjectsPage = () => {
+    const [fullData] = useState(WorkData);
+    const [filter, setFilter] = useState(null);
     const [samples, setSamples] = useState(WorkData);
+
+    useEffect(() => {
+        if(! filter){
+            setSamples(fullData);
+            return;
+        }
+
+        setSamples(fullData.filter(entry => entry.categories.indexOf(filter) !== -1))
+    }, [filter, fullData]);
 
   return (
     <Box>
-        <CategoriesCarousel/>
+        <CategoriesCarousel setFilter={setFilter}/>
+
         <Box sx={{padding: "10px"}}>
             <Typography sx={{color: "#272727"}} variant="h5">Results:</Typography>
         </Box>
